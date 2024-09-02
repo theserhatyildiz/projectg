516edc029b8657b67ce4c20450d4c4e2d4fd84d5
Serhat Yildiz <yildizserhat@yahoo.com>
2024-09-02 16:14:46 +0200

    changes

--- client/src/component/UseAuth.jsx
+++ client/src/component/UseAuth.jsx
@@ -6,7 +6,6 @@ export function useAuth() {
     const storedUser = localStorage.getItem("app-user");
     if (storedUser) {
       try {
-        console.log("Loaded user from localStorage:", storedUser);
         return JSON.parse(storedUser);
       } catch (error) {
         console.error("Invalid user data in localStorage:", error);
@@ -21,8 +20,7 @@ export function useAuth() {
   // Function to fetch CSRF token
   async function fetchCsrfToken() {
     try {
-      console.log("Fetching CSRF token...");
-      const response = await fetch("https://galwinapp-c654a544b729.herokuapp.com/csrf-token", { credentials: 'include' });
+      const response = await fetch("http://localhost:8000/csrf-token", { credentials: 'include' });
       if (response.ok) {
         const { csrfToken } = await response.json();
         console.log('CSRF Token fetched:', csrfToken);
@@ -42,8 +40,7 @@ export function useAuth() {
   // Function to refresh the access token
   async function refreshAccessToken(parsedUser, retries = 3) {
     try {
-      console.log("Refreshing access token...");
-      const response = await fetch('https://galwinapp-c654a544b729.herokuapp.com/refresh-token', {
+      const response = await fetch('http://localhost:8000/refresh-token', {
         method: 'POST',
         headers: {
           "Content-Type": "application/json",
@@ -55,18 +52,15 @@ export function useAuth() {
 
       if (response.ok) {
         const data = await response.json();
-        console.log("Access token refreshed successfully:", data.token);
         if (data.token) {
           const updatedUser = { ...parsedUser, token: data.token };
           localStorage.setItem("app-user", JSON.stringify(updatedUser));
           setLoggedUser(updatedUser);
-          return data.token;  // Return the new token
         }
       } else {
         console.error('Failed to refresh access token:', response.statusText);
         if (response.status === 403) {
           // If the server returns a 403 status, log out the user
-          console.log("Access forbidden, logging out user.");
           setLoggedUser(null);
           localStorage.removeItem("app-user");
         }
@@ -74,63 +68,12 @@ export function useAuth() {
     } catch (error) {
       console.error('Error refreshing access token:', error);
       if (retries > 0) {
-        console.log(`Retrying to refresh access token. Attempts left: ${retries - 1}`);
         setTimeout(() => refreshAccessToken(parsedUser, retries - 1), 2000); // Retry with exponential backoff
       } else {
-        console.log("Failed to refresh token after retries, logging out.");
         setLoggedUser(null);
         localStorage.removeItem("app-user");
       }
     }
-  }
-
-  // Function to fetch data with token handling
-  async function fetchData(url) {
-    const storedUser = localStorage.getItem("app-user");
-    let parsedUser;
-
-    if (storedUser) {
-      parsedUser = JSON.parse(storedUser);
-      const accessToken = parsedUser?.token;
-      console.log("Fetched stored user data:", parsedUser);
-
-      if (accessToken) {
-        const now = Math.floor(Date.now() / 1000);
-        const { exp: accessTokenExp } = jwtDecode(accessToken);
-        console.log(`Access token expires at: ${accessTokenExp}, current time: ${now}`);
-
-        if (now >= accessTokenExp) {
-          console.log("Access token expired, refreshing...");
-          const newToken = await refreshAccessToken(parsedUser);
-          if (!newToken) return;  // Stop if the refresh failed
-          parsedUser.token = newToken;
-        }
-      }
-    }
-
-    // Now fetch the data using the valid token
-    console.log("Fetching data from URL:", url);
-    const response = await fetch(url, {
-      headers: {
-        Authorization: `Bearer ${parsedUser?.token}`,
-        "Content-Type": "application/json"
-      },
-      credentials: 'include'
-    });
-
-    if (!response.ok) {
-      console.error('Failed to fetch data:', response.statusText);
-      if (response.status === 401) {
-        // Token is invalid, try refreshing it again
-        console.log("Received 401, refreshing token...");
-        await refreshAccessToken(parsedUser);
-        return fetchData(url); // Retry after refreshing the token
-      }
-    }
-
-    const data = await response.json();
-    console.log("Data fetched successfully:", data);
-    return data;
   }
 
   // Fetch CSRF token on initial mount
@@ -141,7 +84,6 @@ export function useAuth() {
   // Check and refresh tokens periodically
   useEffect(() => {
     const interval = setInterval(async () => {
-      console.log("Periodic token check...");
       const storedUser = localStorage.getItem("app-user");
       if (storedUser) {
         const parsedUser = JSON.parse(storedUser);
@@ -151,10 +93,8 @@ export function useAuth() {
           const now = Math.floor(Date.now() / 1000);
           const { exp: accessTokenExp } = jwtDecode(accessToken);
           const accessTokenMinutesLeft = (accessTokenExp - now) / 60;
-          console.log(`Access token minutes left: ${accessTokenMinutesLeft}`);
 
-          if (accessTokenMinutesLeft <= 5) { // If the access token is about to expire in the next minute
-            console.log("Access token about to expire, refreshing...");
+          if (accessTokenMinutesLeft <= 1) { // If the access token is about to expire in the next minute
             await refreshAccessToken(parsedUser);
           }
         }
@@ -164,11 +104,10 @@ export function useAuth() {
     return () => clearInterval(interval);
   }, [csrfToken]);
 
-  // Add the session status check after inactivity
+  // Check access token on app load or reopen
   useEffect(() => {
-    // Function to check session status
-    async function checkSessionStatus() {
-      console.log("Checking session status...");
+    // Only run this effect when the CSRF token is available
+    if (csrfToken) {
       const storedUser = localStorage.getItem("app-user");
       if (storedUser) {
         const parsedUser = JSON.parse(storedUser);
@@ -177,35 +116,15 @@ export function useAuth() {
         if (accessToken) {
           const now = Math.floor(Date.now() / 1000);
           const { exp: accessTokenExp } = jwtDecode(accessToken);
-          console.log(`Checking token expiry: now=${now}, exp=${accessTokenExp}`);
 
-          if (now >= accessTokenExp) {
-            // Token has expired, refresh it
-            console.log("Token expired, refreshing...");
-            await refreshAccessToken(parsedUser);
-          } else {
-            // Token is still valid, proceed with normal operation
-            console.log("Token is still valid, continuing...");
+          // If the access token is expired or about to expire, refresh it
+          if (accessTokenExp < now) {
+            refreshAccessToken(parsedUser);
           }
         }
       }
     }
+  }, [csrfToken]); // Run this only when the CSRF token is available
 
-    // Event listener for visibility change
-    function handleVisibilityChange() {
-      if (document.visibilityState === 'visible') {
-        // The app is active again, check the session status
-        console.log("App is active again, checking session status...");
-        checkSessionStatus();
-      }
-    }
-
-    document.addEventListener('visibilitychange', handleVisibilityChange);
-
-    return () => {
-      document.removeEventListener('visibilitychange', handleVisibilityChange);
-    };
-  }, [csrfToken]);
-
-  return { loggedUser, setLoggedUser, csrfToken, fetchData }; // Expose fetchData function
+  return { loggedUser, setLoggedUser, csrfToken };
 }
